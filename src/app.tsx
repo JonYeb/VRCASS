94c46f0a329985af9753f3004535727848a143f7
Jonathan Yeboah <10155597+JonYeb@users.noreply.github.com>
2024-09-23 11:03:45 +0200

    added electron and react typescript boilerplate

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+
+function App() {
+  return (
+    <div>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application.</p>
+    </div>
+  );
+}
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
